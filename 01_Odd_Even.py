ead07af93f5c59a466248722231fa8bbebfdba69
Vaibhav Tripathi <tripathivaibhav795@gmail.com>
2026-09-06 21:33:41 +0530

    Initial commit

---
+++ 01_Odd_Even.py
@@ -0,0 +1,9 @@
+# Program to check whether a number is Odd or Even
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
+    
