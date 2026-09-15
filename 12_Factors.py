d2a5c6aa89ac2c3f190a29103b872e2452ff2f34
Vaibhav Tripathi <tripathivaibhav795@gmail.com>
2026-09-15 13:12:55 +0530

    Added Program of Factors

---
+++ 12_Factors.py
@@ -0,0 +1,9 @@
+# Program of factors of number given by user
+
+num  = int(input("Enter a number: "))
+
+print("Factors of", num, "are: ")
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        print(i, end =" ")
