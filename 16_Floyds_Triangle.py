09792ce5740805ad83c0d3d13d32e66b6eb6d10a
Vaibhav Tripathi <tripathivaibhav795@gmail.com>
2026-09-17 21:20:50 +0530

    Added Floyds triangle Program

---
+++ 16_Floyds_Triangle.py
@@ -0,0 +1,11 @@
+# Program for printing Floyd's triangle
+
+n = int(input("Enter a number: "))
+
+num = 1
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print(num, end = " ")
+        num += 1
+    print()
