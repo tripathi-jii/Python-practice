5fafd2e46ebc45606f8e2469882364bc558d2951
Vaibhav Tripathi <tripathivaibhav795@gmail.com>
2026-09-18 12:49:32 +0530

    Added Diamond Pattern Program

---
+++ 17_Diamond_Pattern.py
@@ -0,0 +1,8 @@
+# Program to print diamond pattern
+
+n = int(input("Enter number of rows: "))
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i) + "*" * (2 * i - 1))    
